c46888b6359289836f10261d58417089b95022ad
gang <ganggstand@gmail.com>
2023-04-07 23:21:54 +0300

    fix admin

--- chat/models.py
+++ chat/models.py
@@ -2,11 +2,28 @@ from datetime import datetime
 
 from django.db import models
 
+
 # Create your models here.
 class Room(models.Model):
     name = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Чат'
+        verbose_name_plural = 'Чаты'
+
+
 class Message(models.Model):
     value = models.CharField(max_length=1000000)
     date = models.DateTimeField(default=datetime.now, blank=True)
     user = models.CharField(max_length=1000000)
     room = models.CharField(max_length=1000000)
+
+    def __str__(self):
+        return f"{self.user} {self.room}"
+
+    class Meta:
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
--- internship/admin.py
+++ internship/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
 from internship.models import Directions
 
 admin.site.register(Directions)
+
+admin.site.site_header = 'HR Администрация'
+admin.site.site_title = 'Стажировка в Банк Центр-инвест'
