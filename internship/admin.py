aa25fe15044d4fd72cf3cc9d7d6db913bd40b8ce
gang <ganggstand@gmail.com>
2023-04-08 10:39:03 +0300

    fix admin

--- internship/admin.py
+++ internship/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 from internship.models import Directions
+from django.contrib.auth.models import Group
 
+admin.site.unregister(Group)
 admin.site.register(Directions)
 
 admin.site.site_header = 'HR Администрация'
